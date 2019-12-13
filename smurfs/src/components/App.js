3daa680ad86972d083fd56d4bc87971e0c093b1f
marielapg <mpgonzales77@gmail.com>
2019-12-13 10:31:41 -0800

    answered the questions/smurfs mvp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
+import DisplaySmurfs from "./displaySmurfs";
+import {Route} from "react-router-dom";
+import Smurfs from "./Smurfs";
+
 class App extends Component {
   render() {
     return (
@@ -8,6 +12,9 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+
+        <Route exact path='/' component ={DisplaySmurfs}/>
+        <Route path ='/smurf/id' component={Smurfs}/>
       </div>
     );
   }
